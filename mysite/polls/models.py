6f9bdb4648ba9e9b164d6939f5fff3cf3b8e1602
vJens <jens.veraa@outlook.com>
2013-03-27 16:57:14 +0100

    P5 - Test a view

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,21 @@
 from django.db import models
-
+import datetime 
+from django.utils import timezone
 # Create your models here.
 
 class Poll(models.Model):
+    def __unicode__(self):
+        return self.question
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        now = timezone.now()
+	return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 class Choice(models.Model):
+    def __unicode__(self):
+        return self.choice_text
     poll = models.ForeignKey(Poll)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
