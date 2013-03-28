83d5b6ea1a545d41efd923dfdbe82d19b894957e
vJens <jens.veraa@outlook.com>
2013-03-28 09:39:46 +0100

    Part 5

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,19 +1,24 @@
 from django.conf.urls import patterns, url
 from django.views.generic import DetailView, ListView
 from polls.models import Poll
+from django.utils import timezone
 
 urlpatterns = patterns('',
     url(r'^$',
         ListView.as_view(
-            queryset=Poll.objects.order_by('-pub_date')[:5],
-            context_object_name='latest_poll_list',
-            template_name='polls/index.html'),
-        name='index'),
+	queryset=Poll.objects.filter(pub_date__lte=timezone.now) \
+            .order_by('-pub_date')[:5],
+        context_object_name='latest_poll_list',
+        template_name='polls/index.html'),
+    name='index'),
+
     url(r'^(?P<pk>\d+)/$',
-        DetailView.as_view(
-            model=Poll,
-            template_name='polls/detail.html'),
-        name='detail'),
+    	DetailView.as_view(
+        queryset=Poll.objects.filter(pub_date__lte=timezone.now),
+        model=Poll,
+        template_name='polls/detail.html'),
+    name='detail'),
+
     url(r'^(?P<pk>\d+)/results/$',
         DetailView.as_view(
             model=Poll,
